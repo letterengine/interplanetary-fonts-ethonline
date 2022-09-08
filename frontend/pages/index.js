1050323ae45dc4adae1ec5af21d77d2c0e4a6b78
JP <jp@animoto.com>
2022-09-07 22:06:02 -0400

    setup monorepo + nextjs

---
+++ frontend/pages/index.js
@@ -0,0 +1,22 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.app}>
+      <Head>
+        <title>InterplanetaryFont</title>
+        <meta name="description" content="Web App created during ETHOnline 2022" />
+        <link rel="apple-touch-icon" href="/logo192.png" />
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <header className={styles.header}>
+        <div className={styles.logoWrap}>
+          <Image src="/logo.svg" className={styles.logo} alt="logo" layout="responsive" width={400} height={400} />
+        </div>
+      </header>
+    </div>
+  );
+}
