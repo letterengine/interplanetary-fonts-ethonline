b128686e68f27ed97efdebb281bac1003281dd9c
JP <jp@animoto.com>
2022-09-21 09:47:04 -0400

    start of superfluid integration

--- contracts/contracts/FontProject.sol
+++ contracts/contracts/FontProject.sol
@@ -3,10 +3,37 @@ pragma solidity ^0.8.9;
 
 import "./InterPlanetaryFontNFT.sol";
 
+import {ISuperfluid, ISuperToken, SuperAppBase, SuperAppDefinitions} from "@superfluid-finance/ethereum-contracts/contracts/apps/SuperAppBase.sol";
+import {IInstantDistributionAgreementV1} from "@superfluid-finance/ethereum-contracts/contracts/interfaces/agreements/IInstantDistributionAgreementV1.sol";
+
+import {IDAv1Library} from "@superfluid-finance/ethereum-contracts/contracts/apps/IDAv1Library.sol";
+
 contract FontProject {
   InterPlanetaryFontNFT private fontNFT = new InterPlanetaryFontNFT();
+
+  /// @notice IDA Library
+  using IDAv1Library for IDAv1Library.InitData;
+  IDAv1Library.InitData internal _idaV1;
   
   mapping(bytes32 => CreateFontProject) public idToFontProject;
+
+  uint32 private currentIDAIndex;
+
+  constructor(ISuperfluid _host) {
+    // IDA Library Initialize.
+    _idaV1 = IDAv1Library.InitData(
+        _host,
+        IInstantDistributionAgreementV1(
+            address(
+                _host.getAgreementClass(
+                    keccak256(
+                        "org.superfluid-finance.agreements.InstantDistributionAgreement.v1"
+                    )
+                )
+            )
+        )
+    );
+  }
 
   event NewFontProjectCreated(
     bytes32 fontId,
@@ -30,7 +57,8 @@ contract FontProject {
     uint256[] mints;
     address[] collaborators;
 
-    uint256 royaltyInstantDistributionId;
+    ISuperToken idaDistributionToken;
+    uint32 royaltyIDAIndex;
     // uint256[] fundingStreamIds; TBD
 
     uint256 createdAt;
@@ -41,6 +69,7 @@ contract FontProject {
     uint256 createdAt,
     uint256 startDateTime,
     uint256 mintPrice,
+    ISuperToken idaDistributionToken,
     string calldata metaDataCID
   ) external {
     bytes32 fontId = keccak256(
@@ -58,16 +87,27 @@ contract FontProject {
     uint256[] memory mints;
     address[] memory collaborators;
 
-    idToFontProject[fontId] = CreateFontProject(
+    currentIDAIndex = currentIDAIndex + 1;
+
+    CreateFontProject memory font = CreateFontProject(
         fontId,
         metaDataCID,
         msg.sender,
         mintPrice,
         mints,
         collaborators,
-        0,
+        idaDistributionToken,
+        currentIDAIndex,
         createdAt,
         startDateTime
+    );
+    idToFontProject[fontId] = font;
+
+    _idaV1.updateSubscriptionUnits(
+      font.idaDistributionToken,
+      font.royaltyIDAIndex,
+      msg.sender,
+      60
     );
 
     emit NewFontProjectCreated(
@@ -87,16 +127,60 @@ contract FontProject {
   ) external payable {
     CreateFontProject storage font = idToFontProject[fontId];
 
-    require(font.startDateTime == 0, "FONT NOT FOUND");
+    require(font.startDateTime != 0, "FONT NOT FOUND");
     require(msg.value == font.mintPrice, "NOT ENOUGH ETH SENT");
 
 
     uint256 tokenId = fontNFT.safeMint(msg.sender, uri);
     font.mints.push(tokenId);
 
+    // TODO - distribute earnings to collaborators
+
     emit FontProjectMinted(
       fontId,
       tokenId
     );
+  }
+
+  function indexOf(uint256[] memory arr, uint256 searchFor) private pure returns (uint256) {
+    for (uint256 i = 0; i < arr.length; i++) {
+      if (arr[i] == searchFor) {
+        return i;
+      }
+    }
+    revert("NOT FOUND");
+  }
+
+  function setFontCollaboratorProfitDistribution(bytes32 fontId, uint128 distributionUnits) public {
+    CreateFontProject memory font = idToFontProject[fontId];
+
+    require(font.startDateTime != 0, "FONT NOT FOUND");
+    require(font.creatorAddress == msg.sender, "ONLY FONT CREATOR CAN UPDATE COLLABORATOR PROFIT DISTRIBUTION");
+
+    for (uint256 i = 0; i < font.collaborators.length; i++) {
+      _idaV1.updateSubscriptionUnits(
+        font.idaDistributionToken,
+        font.royaltyIDAIndex,
+        font.collaborators[i],
+        distributionUnits
+      );
+    }
+  }
+
+  function distributeFontProfit(bytes32 fontId) public {
+    CreateFontProject memory font = idToFontProject[fontId];
+
+    require(font.startDateTime != 0, "FONT NOT FOUND");
+
+    uint256 spreaderTokenBalance = font.idaDistributionToken.balanceOf(address(this));
+
+    (uint256 actualDistributionAmount, ) = _idaV1.ida.calculateDistribution(
+        font.idaDistributionToken,
+        address(this),
+        font.royaltyIDAIndex,
+        spreaderTokenBalance
+    );
+
+    _idaV1.distribute(font.idaDistributionToken, font.royaltyIDAIndex, actualDistributionAmount);
   }
 }
--- contracts/contracts/RoyaltySpreader.sol
+++ contracts/contracts/RoyaltySpreader.sol
@@ -1,6 +1,103 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
+import {ISuperfluid, ISuperToken, SuperAppBase, SuperAppDefinitions} from "@superfluid-finance/ethereum-contracts/contracts/apps/SuperAppBase.sol";
+import {IInstantDistributionAgreementV1} from "@superfluid-finance/ethereum-contracts/contracts/interfaces/agreements/IInstantDistributionAgreementV1.sol";
+
+import {IDAv1Library} from "@superfluid-finance/ethereum-contracts/contracts/apps/IDAv1Library.sol";
+
 contract RoyaltySpreader {
-  // TODO - implement SuperFluid Instant Distribution (IDA) contract
+  /// @notice Super token to be distributed.
+  ISuperToken public spreaderToken;
+
+  /// @notice IDA Library
+  using IDAv1Library for IDAv1Library.InitData;
+  IDAv1Library.InitData internal _idaV1;
+
+  /// @notice Index ID. Never changes.
+  uint32 public constant INDEX_ID = 0;
+
+  constructor(ISuperfluid _host, ISuperToken _spreaderToken) {
+      spreaderToken = _spreaderToken;
+
+      // IDA Library Initialize.
+      _idaV1 = IDAv1Library.InitData(
+          _host,
+          IInstantDistributionAgreementV1(
+              address(
+                  _host.getAgreementClass(
+                      keccak256(
+                          "org.superfluid-finance.agreements.InstantDistributionAgreement.v1"
+                      )
+                  )
+              )
+          )
+      );
+
+      // Creates the IDA Index through which tokens will be distributed
+      _idaV1.createIndex(_spreaderToken, INDEX_ID);
+  }
+
+  // ---------------------------------------------------------------------------------------------
+  // IDA OPERATIONS
+
+  /// @notice Takes the entire balance of the designated spreaderToken in the contract and distributes it out to unit holders w/ IDA
+  function distribute() public {
+      uint256 spreaderTokenBalance = spreaderToken.balanceOf(address(this));
+
+      (uint256 actualDistributionAmount, ) = _idaV1.ida.calculateDistribution(
+          spreaderToken,
+          address(this),
+          INDEX_ID,
+          spreaderTokenBalance
+      );
+
+      _idaV1.distribute(spreaderToken, INDEX_ID, actualDistributionAmount);
+  }
+
+  /// @notice lets an account gain a single distribution unit
+  /// @param subscriber subscriber address whose units are to be incremented
+  function gainShare(address subscriber) public {
+      // Get current units subscriber holds
+      (, , uint256 currentUnitsHeld, ) = _idaV1.getSubscription(
+          spreaderToken,
+          address(this),
+          INDEX_ID,
+          subscriber
+      );
+
+      // Update to current amount + 1
+      _idaV1.updateSubscriptionUnits(
+          spreaderToken,
+          INDEX_ID,
+          subscriber,
+          uint128(currentUnitsHeld + 1)
+      );
+  }
+
+  /// @notice lets an account lose a single distribution unit
+  /// @param subscriber subscriber address whose units are to be decremented
+  function loseShare(address subscriber) public {
+      // Get current units subscriber holds
+      (, , uint256 currentUnitsHeld, ) = _idaV1.getSubscription(
+          spreaderToken,
+          address(this),
+          INDEX_ID,
+          subscriber
+      );
+
+      // Update to current amount - 1 (reverts if currentUnitsHeld - 1 < 0, so basically if currentUnitsHeld = 0)
+      _idaV1.updateSubscriptionUnits(
+          spreaderToken,
+          INDEX_ID,
+          subscriber,
+          uint128(currentUnitsHeld - 1)
+      );
+  }
+
+  /// @notice allows an account to delete its entire subscription this contract
+  /// @param subscriber subscriber address whose subscription is to be deleted
+  function deleteShares(address subscriber) public {
+      _idaV1.deleteSubscription(spreaderToken, address(this), INDEX_ID, subscriber);
+  }
 }
