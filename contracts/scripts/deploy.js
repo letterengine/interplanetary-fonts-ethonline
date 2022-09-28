5b66741317c07662c81a4f1fd819f69fdb52986d
JP <jp@animoto.com>
2022-09-27 20:30:47 -0400

    deploy + verify smart contract

--- contracts/scripts/deploy.js
+++ contracts/scripts/deploy.js
@@ -1,31 +1,40 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
-const hre = require("hardhat");
+const hre = require('hardhat');
+
+const { Framework } = require("@superfluid-finance/sdk-core")
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
-
-  const lockedAmount = hre.ethers.utils.parseEther("1");
-
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log(
-    `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
+  const provider = new ethers.providers.InfuraProvider(
+    "maticmum",
+    process.env.INFURA_ID
   );
+
+  const { chainId } = await provider.getNetwork();
+  console.log({chainId})
+  const sf = await Framework.create({
+    chainId,
+    provider
+  });
+
+
+  const fontProjectFactory = await hre.ethers.getContractFactory("FontProject");
+  const fontProjectContract = await fontProjectFactory.deploy(
+    sf.settings.config.hostAddress,
+    sf.settings.config.idaV1Address,
+    process.env.MATICX_MUMBAI_CONTRACT
+  );
+
+  await fontProjectContract.deployed();
+  console.log("Contract deployed to:", fontProjectContract.address);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+async function runMain() {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+}
+
+runMain();
